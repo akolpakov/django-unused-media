029f11bd9f2ad1f5f5459bc20b21ea4b73d8464d
Andrey Kolpakov <aakolpakov@gmail.com>
2016-03-10 14:24:28 +0100

    bump version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ tests_require = [
 
 setup(
     name='django-unused-media',
-    version='0.1.1',
+    version='0.1.2',
     description='Delete unused media files from Django project',
     long_description=read_md('README.md'),
     keywords='python django unused media remove delete',
